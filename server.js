22b8db8b6a43706cb10bf1d9bc1fb2d82379d1d0
Jacob Menashe <jmenashe@cs.utexas.edu>
2015-09-27 15:37:09 -0500

    Spooky updates.

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express'),
     exphbs  = require('express-handlebars'),
-    config = require('./config');
+    config = require('./config'),
+    path = require('path');
 
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.static('static'));
+app.use(express.static(path.join(__dirname,'static')));
+app.set('views', path.join(__dirname,"views"));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main', 
+  layoutsDir: path.join(__dirname,"views","layouts"),
+  partialsDir: path.join(__dirname,"views","partials")
+}));
+console.error(path.join(__dirname,"views"));
+app.set('remoting', { 
+  context: {enableHttpContext: true}, 
+  errorHandler: { 
+    handler: function handler(err, req, res, callback) {
+      res.send("OPPZ! u spooked da server!?!");
+      res.end();
+  }}
+});
 
 app.get('/', function (req, res) {
   res.render('home', {
